8460c6c4940ba72d42f9cdca0872c0389e4ba614
kmk2000 <kmk2002@gmail.com>
2012-07-28 23:03:35 +0200

    Main script skeleton added

--- solver-python/main.py
+++ solver-python/main.py
@@ -1,1 +1,21 @@
 """Start point of application"""
+from kmk.solver import PuzzleState, SudokuState, AStarSearcher
+import sys
+
+try:
+    while 1:
+        print """
+Select mode: 
+   1: Sudoku 
+   2: Puzzle
+   other: Exit"""
+        input = int(raw_input())
+        if input == 1:
+            print "Sudoku selected"
+        elif input == 2:
+            print "Puzzle selected"
+        else:
+            print "Exiting"
+            break
+except:
+    print "Unexpected error:", sys.exc_info()[0]
